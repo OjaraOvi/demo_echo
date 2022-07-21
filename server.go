fe4e87c793b9ade15d70cc9279549fd11d113dc0
Octavio Jara <octavio.jara@thoughtworks.com>
2022-07-20 21:45:20 -0400

    add database connect

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"myAppEcho/configs"
 	"myAppEcho/controller"
 )
 
 func main() {
 	e := echo.New()
+	//run database
+	configs.ConnectDB()
 	e.GET("/", controller.Index)
 	e.GET("/users", controller.GetUser)
 	e.Logger.Fatal(e.Start(":1323"))
